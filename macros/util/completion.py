3898cd00c1cbf0d5f94796127bfa81ee1a4710f6
Nim'Vek <nimvek@web.de>
2018-11-28 09:07:46 +0100

    minor changes

--- macros/util/completion.py
+++ macros/util/completion.py
@@ -4,17 +4,21 @@ import logging
 __log__ = logging.getLogger(__name__)
 
 
-def _completion(args):
+def completion(args):
    __log__.debug("Start Completion")
    buffer = client.kbhead()
-   wordleft = client.kbwordleft()
+   start = client.kbwordleft()
    __log__.debug(buffer)
-   __log__.debug(wordleft)
-   prefix = buffer[wordleft - 1]
+   __log__.debug(start)
+   prefix = buffer[start - 1] if start > 0 else None
+   start = buffer[start:]
    __log__.debug(prefix)
-   __log__.debug(buffer[wordleft:])
-   __log__.debug(buffer[client.kbwordright(client.kbwordleft(wordleft)):wordleft])
+   __log__.debug(start)
 
-client.evaluate('/def -b"^i" b_completion = /python_call macros.util.completion._completion %{0}')
+client.define(completion)
+#client.define('key_tab','/if (moresize()) ${key_tab}%; /else /completion%; /endif')
+#client.evaluate('/edit key_tab=/if (moresize()) ${key_tab}%; /else /completion%; /endif')
+client.evaluate('/edit key_tab=/if (moresize()) /dokey page%; /else /completion%; /endif')
+#client.evaluate('/def -b"^i" b_completion = /python_call macros.util.completion._completion %{0}')
 
 
